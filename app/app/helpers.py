c004c50a2e4b04759eb50f9b5415bd0a70c87129
Marco Frisoli <marcof787@gmail.com>
2021-06-20 18:10:46 +0100

    refactor: created function to rewrite music db

--- app/app/helpers.py
+++ app/app/helpers.py
@@ -3,6 +3,11 @@ from flask import render_template, current_app
 from .config import config, config_name
 from .extensions import mail
 from threading import Thread
+from app.models import Song
+from app.extensions import db
+import eyed3
+from os import listdir, path
+
 
 def send_async_email(app, msg):
     with app.app_context():
@@ -27,3 +32,43 @@ def send_email(to, subject, template, **kwargs):
     thr.start()
     return thr
 
+
+def rewrite_music_db():
+    
+    Song.query.delete()
+
+    music_dir = current_app.config['MUSIC_DIRECTORY']
+    # Get all file names in music directory
+    song_files = listdir(music_dir)
+    songs_added = 0
+    current_app.logger.info(song_files)
+    for file in song_files:
+
+        if file[-4:] !=".mp3":
+            continue
+
+        current_app.logger.info('Updating songs in Database')
+
+        # Load File
+        audiofile = eyed3.load(path.join(music_dir, file))
+        current_app.logger.info(audiofile.tag.genre)
+        current_app.logger.info(audiofile.tag.getBestDate())
+        audiofile.tag.getBestDate()
+
+        song = Song(
+            song_name = audiofile.tag.title,
+            artist = audiofile.tag.artist,
+            album = audiofile.tag.album or None,
+            year = str(audiofile.tag.getBestDate()) or None,
+            genre = str(audiofile.tag.genre) or None,
+            filename = file
+        )
+        db.session.add(song)
+        db.session.commit()
+        songs_added += 1
+    
+    songs_found = len(song_files)
+
+    message = f"Total files found {songs_found} and songs added {songs_added}"
+
+    return message
